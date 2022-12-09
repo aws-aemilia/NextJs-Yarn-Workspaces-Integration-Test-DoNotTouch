aecd627e56d322c648da2699088bcc17ef91263c
Victor Ding <weikding@amazon.com>
2022-12-09 00:02:01 -0800

    fix

--- packages/web-app/next.config.js
+++ packages/web-app/next.config.js
@@ -3,6 +3,19 @@ const withTM = require('next-transpile-modules')([
   '@project/components',
 ]);
 
+const nextConfig = {
+  experimental: {
+    // this will allow nextjs to resolve files (js, ts, css)
+    // outside packages/app directory. 
+    externalDir: true,
+  },
+};
+
 module.exports = withPlugins([
-  withTM,
+  withTM(
+    // nextjs config
+    {
+      ...nextConfig,
+    }
+  ),
 ]);
